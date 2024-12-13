da7aa59b5a225e6bdd35b3b3f97b973bc53d89a0
nica <nica.dable@gmail.com>
2024-12-13 21:33:21 +0800

    chore: firebase setup

---
+++ repetiq-app/src/utils/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqv6t8LZJ98r0UKRG4x-Zu-MkSGFghabI",
+  authDomain: "repetiq-spaces-repetition-app.firebaseapp.com",
+  projectId: "repetiq-spaces-repetition-app",
+  storageBucket: "repetiq-spaces-repetition-app.firebasestorage.app",
+  messagingSenderId: "996387188224",
+  appId: "1:996387188224:web:2f7a2ac86c1e80061d72e5",
+  measurementId: "G-YVHLP7L45H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
